dd8b63245d6e0030125cfd32e6d0508c093d456d
aayushKatrodiya <aayushkatrodiya8460@gmail.com>
2023-12-23 18:56:50 +0530

    update

--- lib/home_screen/screens/product.dart
+++ lib/home_screen/screens/product.dart
@@ -8,15 +8,57 @@ class ProductScreen extends StatefulWidget {
 }
 
 class _ProductScreenState extends State<ProductScreen> {
+  String profileImage = "assets/image/Profile_image.jpg";
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text("ProductScreen"),
-        ],
+    return Scaffold(
+      appBar: _apBar(context),
+      body: const Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text("ProductScreen"),
+          ],
+        ),
       ),
+    );
+  }
+
+  AppBar _apBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.white,
+      leading: const Icon(
+        Icons.settings_outlined,
+        color: Colors.black,
+        size: 30,
+      ),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.only(right: 15),
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: FittedBox(
+                        child: Image(
+                          image: AssetImage(profileImage),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              });
+            },
+            child: CircleAvatar(
+              backgroundImage: AssetImage(profileImage),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:product_care/add_product_screen/add_product_screen.dart';
 import 'package:product_care/home_screen/home_screen.dart';
+import 'package:product_care/home_screen/screens/product.dart';
 import 'package:product_care/home_screen/screens/serch.dart';
 
 void main() {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: false,
       ),
-      home: const HomeScreen(),
+      home: const ProductScreen(),
       debugShowCheckedModeBanner: false,
       routes: {
         "/homePage": (context) => const HomeScreen(),
