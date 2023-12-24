1ef9cbf1ce6ee5d6861fa0b17679a967714eedab
aayushKatrodiya <aayushkatrodiya8460@gmail.com>
2023-12-24 12:06:47 +0530

    commite

--- lib/home_screen/screens/product.dart
+++ lib/home_screen/screens/product.dart
@@ -14,11 +14,100 @@ class _ProductScreenState extends State<ProductScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _apBar(context),
-      body: const Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: ListView(
           children: [
-            Text("ProductScreen"),
+            const Text(
+              "Products*",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 9),
+            SingleChildScrollView(
+              // scrollDirection: Axis.horizontal,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    height: 40,
+                    width: 140,
+                    decoration: const BoxDecoration(
+                      color: Colors.black54,
+                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        "Expirey Soon",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 40,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      // color: Colors.black54,
+                      border: Border.all(),
+                      borderRadius: const BorderRadius.all(Radius.circular(30)),
+                    ),
+                    child: const Center(
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(
+                            Icons.filter_alt_outlined,
+                            color: Colors.black87,
+                          ),
+                          Text(
+                            "Filter",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.black87,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 40,
+                    width: 120,
+                    decoration: BoxDecoration(
+                      // color: Colors.black54,
+                      border: Border.all(),
+                      borderRadius: const BorderRadius.all(Radius.circular(30)),
+                    ),
+                    child: const Center(
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(
+                            Icons.sort,
+                            color: Colors.black87,
+                          ),
+                          Text(
+                            "Sort by",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.black87,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
